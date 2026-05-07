6f4c7cbcfc8caa8ae709d3b84c516899acc10c82
John Doe <johndoe@example.com>
2026-05-07 23:19:08 +0300

    feat

--- scripts/button.js
+++ scripts/button.js
@@ -1,24 +1,56 @@
 (function () {
-    const TRACKING_DOMAIN = 'https://tracking.ghatgp2o.beget.tech';
+  const SCRIPT_ID = "tracking-script";
 
-    function buildTrackingUrl() {
+  function getScriptConfig() {
+    const script =
+      document.getElementById(SCRIPT_ID) ||
+      document.currentScript;
+    if (!script) {
+      console.error("Tracking script not found");
+      throw new Error("Tracking script not found");
+    }
+
+    return {
+      scriptSubid: script.dataset.subid,
+      scriptTargetUrl: script.dataset.targetUrl,
+      scriptTargetTtp: script.dataset.targetTtp,
+      scriptTargetFbp: script.dataset.targetFbp
+    };
+
+  }
+``
+    function buildTrackingUrl(config) {
       const currentUrl = new URL(window.location.href);
   
-      const trackingUrl = new URL(`${TRACKING_DOMAIN}/api/click`);
+      const trackingUrl = new URL(config.scriptTargetUrl);
   
       currentUrl.searchParams.forEach((value, key) => {
         trackingUrl.searchParams.set(key, value);
       });
 
+      if (config.scriptSubid) {
+        trackingUrl.searchParams.set('subid', config.scriptSubid);
+      }
+
+      if (config.scriptTargetTtp) {
+        trackingUrl.searchParams.set('ttp', config.scriptTargetTtp);
+      }
+
+      if (config.scriptTargetFbp) {
+        trackingUrl.searchParams.set('fbp', config.scriptTargetFbp);
+      }
+
       return trackingUrl.toString();
     }
   
     function patchLinks() {
+      const config = getScriptConfig();
+
       const links = document.querySelectorAll('a[href]');
 
       const buttons = document.querySelectorAll('button[type="button"]');
   
-      const trackingUrl = buildTrackingUrl();
+      const trackingUrl = buildTrackingUrl(config);
       
       [...links, ...buttons].forEach(element => {
         const isLink = element.matches('a[href]');
@@ -31,7 +63,7 @@
 
           element.addEventListener('click', (event) => {
             event.preventDefault();
-            window.open(buildTrackingUrl(), '_blank');
+            window.open(trackingUrl, '_blank');
           });
         }
       });
